512c2aa335dfd41597503509ed8f82362468eb2e
Oksana-Horbachevska <oksanapaolo@gmail.com>
2025-04-27 15:42:33 +0200

    hw-8

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-const liElems = document.querySelectorAll('#categories > li');
-console.log(`Number of categories: ${liElems.length}`);
-
-liElems.forEach(function (elem, index) {
-  console.log(`Category: ${elem.firstElementChild.textContent}`);
-  console.log(`Elements: ${elem.lastElementChild.children.length}`);
-});
